603d8e8e2ae16adbfd19d43bfa874e79f34d266e
Michael F. Weinberg <michael@luckymike.com>
2016-04-15 15:10:23 -0700

    Refactor to support multiple releases for each OS.

--- lib/sparkleformation/registry/official_amis.rb
+++ lib/sparkleformation/registry/official_amis.rb
@@ -1,22 +1,33 @@
 SfnRegistry.register(:official_amis) do |_name, _config = {}|
 
-  type = _config.fetch(:type, 'instance_store').to_sym
-  virt = _config.fetch(:virt, 'hvm').to_sym
-  os_versions = _config.fetch(:os_versions, ['trusty', 'ubuntu_12', 'amazon_2015_09'])
+  type = _config.fetch(:type, 'instance')
+  virt = _config.fetch(:virt, 'hvm')
+  os_versions = _config.fetch(:os_versions, ['xenial', 'trusty', 'precise', 'amazon'])
+  ami_release = _config.fetch(:ami_release, 'latest')
 
   ami_hash = {}
 
   os_versions.each do |os|
-    ami_hash[os] = registry!("#{os}_amis".to_sym)
+    release_hash = registry!("#{os}_amis".to_sym)
+    if ami_release == 'latest'
+      latest_release = release_hash['latest']
+      ami_hash[os] = release_hash['releases'][latest_release]
+    else
+      ami_hash[os] = release_hash['releases'][ami_release]
+    end
   end
-  
-  ami_hash.each do |release, info|
-    info[:amis].each do |region, ids|
-      mappings do
-        official_amis do
-          camel_keys_set!(:auto_disable)
-          set!(region) do
-            set!(info[:version], ids[virt][type])
+
+  ami_hash.each do |version, info|
+    info['amis'].each do |region, ids|
+      unless ids[virt].nil?
+        unless ids[virt][type].nil?
+          mappings do
+            official_amis do
+              camel_keys_set!(:auto_disable)
+              set!(region) do
+                set!(version, ids[virt][type])
+              end
+            end
           end
         end
       end
